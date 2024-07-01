3611b55f3ae89173128c1d0289475de3d9a97342
Jeremy Corbett <jlcorbet@amazon.com>
2024-07-01 16:30:41 -0700

    Revert "Oof"
    This reverts commit 51e713b0fbcd6ab2a0f83bdb1e9e407943bf3028.

--- spec/cassandra/cluster/schema/fetchers_spec.rb
+++ spec/cassandra/cluster/schema/fetchers_spec.rb
@@ -17,7 +17,6 @@
 #++
 
 require 'spec_helper'
-require 'byebug'
 
 module Cassandra
   class Cluster
@@ -32,12 +31,7 @@ module Cassandra
         ].each do |(version, klass, parser_class)|
 
           describe(klass) do
-            let(:data) do 
-              f = File.read(File.dirname(__FILE__) + '/fetchers/' + version + '-data.json')
-              puts f.class
-              byebug
-              JSON.load(f.to_s) 
-            end
+            let(:data) { JSON.load(File.read(File.dirname(__FILE__) + '/fetchers/' + version + '-data.json')) }
             let(:connection) { double('cql protocol handler') }
             let(:schema_type_parser) {
               parser_class.new
